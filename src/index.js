6708f3967b9d3325ea21b301251386bd459d9497
Mitchell Chan <michan@Mitchells-MBP.hitronhub.home>
2020-08-16 21:07:18 -0700

    Introduction to React - Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const App = () => {
+	const now = new Date()
+	const a = 10
+	const b = 20
+	console.log("Hello from component")
+  	return (
+    	<div>
+      		<p>Hello world, it is {now.toString()}</p>
+      		<p>
+      			{a} plus {b} is {a + b}
+      		</p>
+    	</div>
+  	)
+}
+
+/* 
 const App = () => (
   <div>
     <p>Hello world</p>
   </div>
 )
+ */
 
 ReactDOM.render(<App />, document.getElementById('root'))
